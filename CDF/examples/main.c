f77be5576f9088385dfbd716cff1ba0f89d22115
Yuki-F-HCU <34057035+Yuki-F-HCU@users.noreply.github.com>
2018-07-06 01:58:24 +0900

    Add files via upload

---
+++ CDF/examples/main.c
@@ -0,0 +1,354 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ * File: main.c
+ *
+ * MATLAB Coder version            : 3.2
+ * C/C++ source code generated on  : 05-Jun-2018 22:14:08
+ */
+
+/*************************************************************************/
+/* This automatically generated example C main file shows how to call    */
+/* entry-point functions that MATLAB Coder generated. You must customize */
+/* this file for your application. Do not modify this file directly.     */
+/* Instead, make a copy of this file, modify it, and integrate it into   */
+/* your development environment.                                         */
+/*                                                                       */
+/* This file initializes entry-point function arguments to a default     */
+/* size and value before calling the entry-point functions. It does      */
+/* not store or use any values returned from the entry-point functions.  */
+/* If necessary, it does pre-allocate memory for returned values.        */
+/* You can use this file as a starting point for a main function that    */
+/* you can deploy in your application.                                   */
+/*                                                                       */
+/* After you copy the file, and before you deploy it, you must make the  */
+/* following changes:                                                    */
+/* * For variable-size function arguments, change the example sizes to   */
+/* the sizes that your application requires.                             */
+/* * Change the example values of function arguments to the values that  */
+/* your application requires.                                            */
+/* * If the entry-point functions return values, store these values or   */
+/* otherwise use them as required by your application.                   */
+/*                                                                       */
+/*************************************************************************/
+/* Include Files */
+#include "rt_nonfinite.h"
+#include "BETACDF.h"
+#include "BETACDF_UPPER.h"
+#include "BINOCDF.h"
+#include "BINOCDF_UPPER.h"
+#include "CHI2CDF.h"
+#include "CHI2CDF_UPPER.h"
+#include "EVCDF.h"
+#include "EXPCDF.h"
+#include "FCDF.h"
+#include "FCDF_UPPER.h"
+#include "GAMCDF.h"
+#include "LOGNCDF.h"
+#include "NORMCDF.h"
+#include "TCDF.h"
+#include "TCDF_UPPER.h"
+#include "UNIFCDF.h"
+#include "UNIFCDF_UPPER.h"
+#include "main.h"
+#include "BETACDF_terminate.h"
+#include "BETACDF_initialize.h"
+
+/* Function Declarations */
+static double argInit_real_T(void);
+static void main_BETACDF(void);
+static void main_BETACDF_UPPER(void);
+static void main_BINOCDF(void);
+static void main_BINOCDF_UPPER(void);
+static void main_CHI2CDF(void);
+static void main_CHI2CDF_UPPER(void);
+static void main_EVCDF(void);
+static void main_EXPCDF(void);
+static void main_FCDF(void);
+static void main_FCDF_UPPER(void);
+static void main_GAMCDF(void);
+static void main_LOGNCDF(void);
+static void main_NORMCDF(void);
+static void main_TCDF(void);
+static void main_TCDF_UPPER(void);
+static void main_UNIFCDF(void);
+static void main_UNIFCDF_UPPER(void);
+
+/* Function Definitions */
+
+/*
+ * Arguments    : void
+ * Return Type  : double
+ */
+static double argInit_real_T(void)
+{
+  return 0.0;
+}
+
+/*
+ * Arguments    : void
+ * Return Type  : void
+ */
+static void main_BETACDF(void)
+{
+  double c;
+
+  /* Initialize function 'BETACDF' input arguments. */
+  /* Call the entry-point 'BETACDF'. */
+  c = BETACDF(argInit_real_T(), argInit_real_T(), argInit_real_T());
+}
+
+/*
+ * Arguments    : void
+ * Return Type  : void
+ */
+static void main_BETACDF_UPPER(void)
+{
+  double c;
+
+  /* Initialize function 'BETACDF_UPPER' input arguments. */
+  /* Call the entry-point 'BETACDF_UPPER'. */
+  c = BETACDF_UPPER(argInit_real_T(), argInit_real_T(), argInit_real_T());
+}
+
+/*
+ * Arguments    : void
+ * Return Type  : void
+ */
+static void main_BINOCDF(void)
+{
+  double c;
+
+  /* Initialize function 'BINOCDF' input arguments. */
+  /* Call the entry-point 'BINOCDF'. */
+  c = BINOCDF(argInit_real_T(), argInit_real_T(), argInit_real_T());
+}
+
+/*
+ * Arguments    : void
+ * Return Type  : void
+ */
+static void main_BINOCDF_UPPER(void)
+{
+  double c;
+
+  /* Initialize function 'BINOCDF_UPPER' input arguments. */
+  /* Call the entry-point 'BINOCDF_UPPER'. */
+  c = BINOCDF_UPPER(argInit_real_T(), argInit_real_T(), argInit_real_T());
+}
+
+/*
+ * Arguments    : void
+ * Return Type  : void
+ */
+static void main_CHI2CDF(void)
+{
+  double c;
+
+  /* Initialize function 'CHI2CDF' input arguments. */
+  /* Call the entry-point 'CHI2CDF'. */
+  c = CHI2CDF(argInit_real_T(), argInit_real_T());
+}
+
+/*
+ * Arguments    : void
+ * Return Type  : void
+ */
+static void main_CHI2CDF_UPPER(void)
+{
+  double c;
+
+  /* Initialize function 'CHI2CDF_UPPER' input arguments. */
+  /* Call the entry-point 'CHI2CDF_UPPER'. */
+  c = CHI2CDF_UPPER(argInit_real_T(), argInit_real_T());
+}
+
+/*
+ * Arguments    : void
+ * Return Type  : void
+ */
+static void main_EVCDF(void)
+{
+  double c;
+
+  /* Initialize function 'EVCDF' input arguments. */
+  /* Call the entry-point 'EVCDF'. */
+  c = EVCDF(argInit_real_T(), argInit_real_T(), argInit_real_T());
+}
+
+/*
+ * Arguments    : void
+ * Return Type  : void
+ */
+static void main_EXPCDF(void)
+{
+  double c;
+
+  /* Initialize function 'EXPCDF' input arguments. */
+  /* Call the entry-point 'EXPCDF'. */
+  c = EXPCDF(argInit_real_T(), argInit_real_T());
+}
+
+/*
+ * Arguments    : void
+ * Return Type  : void
+ */
+static void main_FCDF(void)
+{
+  double c;
+
+  /* Initialize function 'FCDF' input arguments. */
+  /* Call the entry-point 'FCDF'. */
+  c = FCDF(argInit_real_T(), argInit_real_T(), argInit_real_T());
+}
+
+/*
+ * Arguments    : void
+ * Return Type  : void
+ */
+static void main_FCDF_UPPER(void)
+{
+  double c;
+
+  /* Initialize function 'FCDF_UPPER' input arguments. */
+  /* Call the entry-point 'FCDF_UPPER'. */
+  c = FCDF_UPPER(argInit_real_T(), argInit_real_T(), argInit_real_T());
+}
+
+/*
+ * Arguments    : void
+ * Return Type  : void
+ */
+static void main_GAMCDF(void)
+{
+  double c;
+
+  /* Initialize function 'GAMCDF' input arguments. */
+  /* Call the entry-point 'GAMCDF'. */
+  c = GAMCDF(argInit_real_T(), argInit_real_T(), argInit_real_T());
+}
+
+/*
+ * Arguments    : void
+ * Return Type  : void
+ */
+static void main_LOGNCDF(void)
+{
+  double c;
+
+  /* Initialize function 'LOGNCDF' input arguments. */
+  /* Call the entry-point 'LOGNCDF'. */
+  c = LOGNCDF(argInit_real_T(), argInit_real_T(), argInit_real_T());
+}
+
+/*
+ * Arguments    : void
+ * Return Type  : void
+ */
+static void main_NORMCDF(void)
+{
+  double c;
+
+  /* Initialize function 'NORMCDF' input arguments. */
+  /* Call the entry-point 'NORMCDF'. */
+  c = NORMCDF(argInit_real_T(), argInit_real_T(), argInit_real_T());
+}
+
+/*
+ * Arguments    : void
+ * Return Type  : void
+ */
+static void main_TCDF(void)
+{
+  double c;
+
+  /* Initialize function 'TCDF' input arguments. */
+  /* Call the entry-point 'TCDF'. */
+  c = TCDF(argInit_real_T(), argInit_real_T());
+}
+
+/*
+ * Arguments    : void
+ * Return Type  : void
+ */
+static void main_TCDF_UPPER(void)
+{
+  double c;
+
+  /* Initialize function 'TCDF_UPPER' input arguments. */
+  /* Call the entry-point 'TCDF_UPPER'. */
+  c = TCDF_UPPER(argInit_real_T(), argInit_real_T());
+}
+
+/*
+ * Arguments    : void
+ * Return Type  : void
+ */
+static void main_UNIFCDF(void)
+{
+  double c;
+
+  /* Initialize function 'UNIFCDF' input arguments. */
+  /* Call the entry-point 'UNIFCDF'. */
+  c = UNIFCDF(argInit_real_T(), argInit_real_T(), argInit_real_T());
+}
+
+/*
+ * Arguments    : void
+ * Return Type  : void
+ */
+static void main_UNIFCDF_UPPER(void)
+{
+  double c;
+
+  /* Initialize function 'UNIFCDF_UPPER' input arguments. */
+  /* Call the entry-point 'UNIFCDF_UPPER'. */
+  c = UNIFCDF_UPPER(argInit_real_T(), argInit_real_T(), argInit_real_T());
+}
+
+/*
+ * Arguments    : int argc
+ *                const char * const argv[]
+ * Return Type  : int
+ */
+int main(int argc, const char * const argv[])
+{
+  (void)argc;
+  (void)argv;
+
+  /* Initialize the application.
+     You do not need to do this more than one time. */
+  BETACDF_initialize();
+
+  /* Invoke the entry-point functions.
+     You can call entry-point functions multiple times. */
+  main_BETACDF();
+  main_BETACDF_UPPER();
+  main_BINOCDF();
+  main_BINOCDF_UPPER();
+  main_CHI2CDF();
+  main_CHI2CDF_UPPER();
+  main_EVCDF();
+  main_EXPCDF();
+  main_FCDF();
+  main_FCDF_UPPER();
+  main_GAMCDF();
+  main_LOGNCDF();
+  main_NORMCDF();
+  main_TCDF();
+  main_TCDF_UPPER();
+  main_UNIFCDF();
+  main_UNIFCDF_UPPER();
+
+  /* Terminate the application.
+     You do not need to do this more than one time. */
+  BETACDF_terminate();
+  return 0;
+}
+
+/*
+ * File trailer for main.c
+ *
+ * [EOF]
+ */
